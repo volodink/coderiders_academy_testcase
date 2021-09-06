7fd2ccdfe7a06e8a54c18e6d2ef5b6999541755c
volodink <volodin.konstantin@gmail.com>
2021-09-06 14:07:19 +0300

    Добавлена реализация Задачи 1.

---
+++ task01/task01.py
@@ -0,0 +1,8 @@
+import random
+import sort
+
+random_list = [random.randint(0, 10) for _ in range(25)]
+
+print(f'Random list =>               {random_list}')
+print(f'Sorted random list =>        {sorted(random_list)}')
+print(f'Quick sort implementation => {sort.qsort(random_list)}')
