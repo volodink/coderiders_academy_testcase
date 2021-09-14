371f6b6231956d3faeadede7f6694264f22deb57
volodink <volodin.konstantin@gmail.com>
2021-09-14 13:24:36 +0300

    Очистка репозитория для работы с примерами на Java.

--- task03/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='coderiders_academy_testcase_3',
-      version='0.1',
-      description='Coderiders Academy Task 3: Elevators',
-      author='Konstantin Volodin',
-      author_email='volodin.konstantin@gmail.com',
-      packages=find_packages()
-)
